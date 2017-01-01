80902dc55818f67de4c3e13889948be187efedcc
Monroe Mann <findingsummer@aol.com>
2017-01-01 02:05:26 +0000

    added validation to the form

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+  validates :saying, presence: true, length: {maximum: 1000, minimum: 5}
+  validates :author, presence: true, length: {maximum: 100, minimum: 3}
 end
